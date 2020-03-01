b38aa557770824e65d5db2f6ca7b19ad5864a865
Maverick <anuragr741@gmail.com>
2020-03-01 10:06:07 +0530

    10am

--- app/src/main/java/com/example/debugnote/MainActivity.java
+++ app/src/main/java/com/example/debugnote/MainActivity.java
@@ -8,10 +8,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +23,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
         setContentView(R.layout.activity_main);
         pdfListView = (ListView) findViewById(R.id.myPDFList);
 
-        String[] pdfFiles = {"Physics", "Chemistry", "Computer", "Circuits","DataStructures","Electrical"};
+
+
+
+        String[] pdfFiles = {"Physics(Applied Sciences)", "Chemistry(Applied Sciences)", "Computer(Applied Sciences)","Electrical(Applied Sciences)","Maths1(Applied Sciences)","Maths2(Applied Sciences)","Data Structures(CSE)","Computer Architecture(CSE)","Maths4(CSE)", "Analog Circuits(ECE)","Electronic Devices(ECE)","Network Analysis(ECE)"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,pdfFiles){
 
--- app/src/main/java/com/example/debugnote/PDFOpener.java
+++ app/src/main/java/com/example/debugnote/PDFOpener.java
@@ -3,6 +3,7 @@ package com.example.debugnote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.github.barteksc.pdfviewer.PDFView;
 
@@ -13,35 +14,36 @@ public class PDFOpener extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
         setContentView(R.layout.activity_pdfopener);
 
         myPDFViewer = (PDFView) findViewById(R.id.pdfViewer);
 
         String getItem = getIntent().getStringExtra("pdfFileName");
 
-        if(getItem.equals("Physics"))
+        if(getItem.equals("Physics(Applied Sciences)"))
         {
-            myPDFViewer.fromAsset("physics module 2.pdf");
+            myPDFViewer.fromAsset("physics module 2.pdf").load();
         }
-        if(getItem.equals("Chemistry"))
+        if(getItem.equals("Chemistry(Applied Sciences)"))
         {
-            myPDFViewer.fromAsset("chemistry module 1.pdf");
+            myPDFViewer.fromAsset("chemistry module 1.pdf").load();
         }
-        if(getItem.equals("Computer"))
+        if(getItem.equals("Computer(Applied Sciences)"))
         {
-            myPDFViewer.fromAsset("c programming module 1.pdf");
+            myPDFViewer.fromAsset("c programming module 1.pdf").load();
         }
-        if(getItem.equals("Circuits"))
+        if(getItem.equals("Analog Circuits(ECE)"))
         {
-            myPDFViewer.fromAsset("ANALOG CIRCUITS UNIT 3.pdf");
+            myPDFViewer.fromAsset("ANALOG CIRCUITS UNIT 3.pdf").load();
         }
-        if(getItem.equals("DataStructures"))
+        if(getItem.equals("DataStructures(CSE)"))
         {
-            myPDFViewer.fromAsset("Stack Notes.pdf");
+            myPDFViewer.fromAsset("Stack Notes.pdf").load();
         }
-        if(getItem.equals("Electrical"))
+        if(getItem.equals("Electrical(Applied Sciences)"))
         {
-            myPDFViewer.fromAsset("physcis module 5.pdf");
+            myPDFViewer.fromAsset("physcis module 5.pdf").load();
         }
     }
 }
